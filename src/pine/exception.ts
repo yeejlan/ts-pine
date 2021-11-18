00767d4f2dba38695cee04511c8d066807bd530d
yufang <yufang@retaileye.ai>
2021-11-18 16:49:25 +0800

    add more exception

--- src/pine/exception.ts
+++ src/pine/exception.ts
@@ -1,6 +1,11 @@
+export class PineException {};
+
 export class AuthException {};
 export class ControllerException {};
 export class DbException {};
-export class PineException {};
 export class ServiceException {};
 export class UserException {};
+
+export class NotFoundException {};
+export class InterServerErrorException {};
+
